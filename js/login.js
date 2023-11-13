0fed425390080eed1886dec9b578add8ccd6f16c
ShovanTheMan <shovan2192@gmail.com>
2023-11-14 02:31:32 +0600

    deposit added

--- js/login.js
+++ js/login.js
@@ -2,10 +2,15 @@ console.log('login in js file');
 document.getElementById('btn-submit').addEventListener('click',function(){
     const emailField = document.getElementById('user-email');
     const email = emailField.value;
-    console.log(email);
-})
-document.getElementById('btn-submit').addEventListener('click',function(){
+
     const passField = document.getElementById('user-pass');
-    const pass = passField.value;
-    console.log(pass);
+    const password = passField.value;
+
+    if(email === 'sonaton@baap.com' && password === 'secret'){
+        window.location.href = 'bank.html';
+    }
+    else{
+        alert('invalid user');
+    }
+    
 })
