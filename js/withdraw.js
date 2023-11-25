d7404e0997eae5f8ba278ed9fa4fecc7ab039481
ShovanTheMan <shovan2192@gmail.com>
2023-11-25 22:39:36 +0600

    done added

--- js/withdraw.js
+++ js/withdraw.js
@@ -5,14 +5,33 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
     const newWithDrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithDrawAmountString);
 
+    withdrawField.value = '';
+    if(isNaN(newWithdrawAmount)){
+        alert('Please provide a valid number');
+        return;
+    }
+
 
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
 
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    withdrawField.value = '';
+    if(newWithdrawAmount > previousBalanceTotal){
+        alert('Do not Cheating!! We see YOU');
+        return;
+    }
     const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
     withdrawTotalElement.innerText = currentWithdrawTotal;
 
-    withdrawField.value = '';
+
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+
+    
 })
